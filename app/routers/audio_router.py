98fb95a6213c72d6ed9786a7bf22bbff92960fd5
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-28 22:24:55 +0500

    Created router for audio

---
+++ app/routers/audio_router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, UploadFile, File, Form
+
+from app.repositories.upload_audio_repo import ua_repo
+from app.schemas.response_schemas import Sch_Upload_Audio
+
+audio_router = APIRouter(
+    tags=["🎼 Upload Audio"],
+    prefix="/audio",
+)
+
+
+@audio_router.post("/upload/")
+async def upload_audio(
+    file: UploadFile = File(...),
+    custom_name: str = Form(...),
+) -> Sch_Upload_Audio:
+    file_extension = file.filename.split(".")[-1].lower()
+
+    # Проверка на допустимость имени файла и расширения файла
+    ua_repo.check_valid_name(custom_name)
+    ua_repo.check_valid_extension(file_extension)
+
+    file_location = f"audio_storage/{custom_name}.{file_extension}"
+
+    # Сохранение файла на диск
+    await ua_repo.save_audio(file, file_location)
+
+    return {"filename": custom_name, "path": file_location}
