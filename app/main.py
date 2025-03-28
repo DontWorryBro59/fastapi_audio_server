6f51410b689f295911fe8474b14f0a00e4d8630d
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-28 22:53:20 +0500

    Upgrade main.py, added lifespan, include_router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+import os
+from contextlib import asynccontextmanager
+
 import uvicorn
+from fastapi import FastAPI
 
-app = FastAPI()
+from app.config.app_config import AUDIO_STORAGE_PATH
+from app.routers.audio_router import audio_router
 
-@app.get('/')
-async def index():
-    return {'message': 'Base API file'}
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', debug=True)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Убедимся, что директория для сохранения аудио файлов существует
+    if not os.path.exists(AUDIO_STORAGE_PATH):
+        os.makedirs(AUDIO_STORAGE_PATH)
+    # тут мы будем инициализировать базу данных
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(audio_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
