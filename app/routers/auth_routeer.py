b1ae1b2ac8a9a65b337055a1a7072da92fb1bc6a
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-29 14:21:31 +0500

    Added new routers for auth

--- app/routers/auth_routeer.py
+++ app/routers/auth_routeer.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+import jwt
+from fastapi import APIRouter, HTTPException, Body
 from fastapi.responses import JSONResponse
 
 from app.config.app_config import settings
@@ -17,11 +18,51 @@ async def yandex_auth():
 @auth_router.get("/yandex/callback")
 async def yandex_callback(code: str, cid: str = None):
     token_data = await AuthRepo.get_yandex_token(code)
-
     if token_data.get("error"):
         return JSONResponse(content={"error": token_data.get("error")}, status_code=400)
-    access_token = token_data.get("access_token")
-    refresh_token = token_data.get("refresh_token")
-    print(access_token)
-    print(refresh_token)
 
+    # Получаем токен пользователя
+    access_token = token_data.get("access_token")
+
+    if not access_token:
+        return JSONResponse(
+            content={"error": "Не удалось получить токены"}, status_code=400
+        )
+    # Получаем User-ID пользователя с помощью токена
+    user_info = await AuthRepo.get_user_info(access_token)
+    user_id = user_info.get("id")
+
+    if not user_id:
+        return JSONResponse(
+            content={"error": "Не удалось получить ID пользователя"}, status_code=400
+        )
+
+    # Создаём внутренние JWT токены на основе User-ID
+    new_access_token, new_refresh_token = AuthRepo.create_jwt_tokens(user_id)
+
+    return {
+        "user_id": user_id,
+        "access_token": new_access_token,
+        "refresh_token": new_refresh_token,
+    }
+
+
+@auth_router.post("/refresh")
+async def refresh_token(refr_token: str):
+    """Обновляет access_token по refresh_token"""
+    print(refr_token)
+    try:
+        payload = jwt.decode(refr_token, settings.SECRET_KEY, algorithms=["HS256"])
+        user_id = payload.get("user_id")
+        type_token = payload.get("type")
+        if not user_id or type_token != "refresh":
+            raise HTTPException(status_code=400, detail="Некорректный refresh_token")
+
+        new_access_token, new_refresh_token = AuthRepo.create_jwt_tokens(user_id)
+        return {"access_token": new_access_token, "refresh_token": new_refresh_token}
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Refresh token истек")
+
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Некорректный refresh_token")
