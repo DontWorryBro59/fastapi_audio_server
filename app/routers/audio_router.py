3197f3a5afd71c05a40a54561328e9e016424392
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-29 14:21:48 +0500

    Added new logic dir

--- app/routers/audio_router.py
+++ app/routers/audio_router.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter, UploadFile, File, Form
+import os
+
+from fastapi import APIRouter, UploadFile, File, Form, Depends
+from fastapi.security import OAuth2PasswordBearer
 
 from app.repositories.upload_audio_repo import ua_repo
 from app.schemas.response_schemas import Sch_Upload_Audio
+from app.repositories.auth_router_repo import AuthRepo
+from config.app_config import AUDIO_STORAGE_PATH
 
 audio_router = APIRouter(
     tags=["🎼 Upload Audio"],
@@ -13,14 +18,21 @@ audio_router = APIRouter(
 async def upload_audio(
     file: UploadFile = File(...),
     custom_name: str = Form(...),
+    user_info: str = Depends(AuthRepo.get_current_user),
 ) -> Sch_Upload_Audio:
+
     file_extension = file.filename.split(".")[-1].lower()
+
+    user_id = user_info["user_id"]
 
     # Проверка на допустимость имени файла и расширения файла
     ua_repo.check_valid_name(custom_name)
     ua_repo.check_valid_extension(file_extension)
+    # Создание директории пользователя, если она не существует
+    if not os.path.exists(f"{AUDIO_STORAGE_PATH}/{user_id}/"):
+        os.makedirs(f"{AUDIO_STORAGE_PATH}/{user_id}/")
 
-    file_location = f"audio_storage/{custom_name}.{file_extension}"
+    file_location = f"{AUDIO_STORAGE_PATH}/{user_id}/{custom_name}.{file_extension}"
 
     # Сохранение файла на диск в директории
     await ua_repo.save_audio(file, file_location)
