d92eff737356cff0a8abadbb8083591c4157e3e8
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-29 20:12:00 +0500

    Change DB model, start working with DB here

--- app/models/users.py
+++ app/models/users.py
@@ -1,23 +1,30 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from app.models.base_model import Base
 
 
 class UserORM(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    audio_files = relationship("AudioFile", back_populates="owner")
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    yandex_id: Mapped[int] = mapped_column(
+        Integer, unique=True, index=True, nullable=True
+    )
+    username: Mapped[str] = mapped_column(String, index=True, nullable=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=True)
+
+    audio_files: Mapped[list["AudioFileORM"]] = relationship(
+        "AudioFileORM", back_populates="owner"
+    )
 
 
 class AudioFileORM(Base):
     __tablename__ = "audio_files"
-    id = Column(Integer, primary_key=True, index=True)
-    filename = Column(String, index=True)
-    file_path = Column(String)
-    user_id = Column(Integer, ForeignKey("users.id"))
 
-    owner = relationship("User", back_populates="audio_files")
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    filename: Mapped[str] = mapped_column(String, index=True)
+    file_path: Mapped[str] = mapped_column(String)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+
+    owner: Mapped["UserORM"] = relationship("UserORM", back_populates="audio_files")
