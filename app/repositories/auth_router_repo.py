9e87e5c0fc8620ae530fce0cf83ba860c6458322
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-29 12:38:13 +0500

    Added method get_user_info

--- app/repositories/auth_router_repo.py
+++ app/repositories/auth_router_repo.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timedelta
+
 import aiohttp
+import jwt
 from fastapi import HTTPException
 
 from app.config.app_config import settings
@@ -19,6 +22,26 @@ class AuthRepo:
         async with aiohttp.ClientSession() as session:
             try:
                 async with session.post(url, data=data) as response:
+                    response.raise_for_status()
+                    json_data = await response.json()
+                    if not json_data:
+                        raise HTTPException(
+                            status_code=500, detail="Пустой ответ от Яндекса"
+                        )
+                    return json_data
+            except aiohttp.ClientResponseError as e:
+                raise HTTPException(
+                    status_code=e.status, detail=f"Ошибка Яндекса: {e.message}"
+                )
+
+    @classmethod
+    async def get_user_info(cls, access_token):
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(
+                    "https://login.yandex.ru/info",
+                    headers={"Authorization": f"OAuth {access_token}"},
+                ) as response:
                     response.raise_for_status()
                     json_data = await response.json()
                     if not json_data:
