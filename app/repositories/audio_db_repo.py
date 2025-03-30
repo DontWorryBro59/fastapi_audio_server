e6d6531c6e0d59efe29ca5585434ef51d18e4ce3
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-30 09:37:07 +0500

    Del dev prints

--- app/repositories/audio_db_repo.py
+++ app/repositories/audio_db_repo.py
@@ -11,14 +11,10 @@ class AudioFileDB:
     async def create_audio(
         cls, yandex_id: str, filename: str, file_path: str, session: AsyncSession
     ):
-        print("creating audio")
-        print(yandex_id)
-        print(filename)
-        print(file_path)
         query = select(UserORM).where(UserORM.yandex_id == yandex_id)
         user = await session.execute(query)
         user = user.scalar_one_or_none()
-        print(user)
+
         if user is None:
             raise HTTPException(status_code=404, detail="User not found")
 
