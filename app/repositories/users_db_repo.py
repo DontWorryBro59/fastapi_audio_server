a46a2c1927f7997daf56946052161db4efa9ab36
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-30 09:50:15 +0500

    Added typing

--- app/repositories/users_db_repo.py
+++ app/repositories/users_db_repo.py
@@ -3,27 +3,27 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.users import UserORM
-from schemas.schemas import SchUpdateUser
+from app.schemas.schemas import SchUpdateUser, SchMessage
 
 
 class UserDB:
 
     @classmethod
-    async def get_user(cls, user_id: int, session: AsyncSession):
+    async def get_user(cls, user_id: int, session: AsyncSession) -> UserORM | None:
         query = select(UserORM).where(UserORM.id == user_id)
         user = await session.execute(query)
         user = user.scalar_one_or_none()
         return user
 
     @classmethod
-    async def get_user_by_yandex_id(cls, yandex_id: int, session: AsyncSession):
+    async def get_user_by_yandex_id(cls, yandex_id: int, session: AsyncSession) -> UserORM | None:
         query = select(UserORM).where(UserORM.yandex_id == yandex_id)
         user = await session.execute(query)
         user = user.scalar_one_or_none()
         return user
 
     @classmethod
-    async def create_user(cls, session: AsyncSession, user_data):
+    async def create_user(cls, session: AsyncSession, user_data) -> dict:
         new_user = UserORM(**user_data)
         session.add(new_user)
         await session.commit()
@@ -32,7 +32,7 @@ class UserDB:
     @classmethod
     async def change_user(
         cls, yandex_id: int, user_data: SchUpdateUser, session: AsyncSession
-    ):
+    ) -> dict:
         user = await cls.get_user_by_yandex_id(yandex_id, session)
 
         user_data = user_data.model_dump(exclude_none=True)
