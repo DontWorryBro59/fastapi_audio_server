4101adc228de59521cadf8c007f50a7cf21c3dc0
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-03-30 09:37:39 +0500

    change response_schemas to schemas

---
+++ app/schemas/schemas.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+class ConfigResponse(BaseModel):
+    class Config:
+        from_attributes = True
+
+
+class SchUploadAudio(ConfigResponse):
+    filename: str
+    path: str
+
+
+class SchGetUser(ConfigResponse):
+    username: str = Field()
+    email: EmailStr
+    yandex_id: str
+
+
+class SchUpdateUser(BaseModel):
+    username: Optional[str] = Field(default=None, max_length=255, min_length=3)
+    email: Optional[EmailStr] = Field(default=None, max_length=255, min_length=3)
